bf6cccf1c5e995c2658049789025a4f2fba45b3a
kiehp <kierra.nelson@aol.com>
2021-10-25 23:26:04 -0400

    removed dist dir from .gitignore file

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ function filterByQuery(query, animalsArray) {
 
 //route
 //the function will take req.query as an arg and filter through animals accordingly
-app.get('/api/animals', (req,res) => {
+app.get('/api/animals.json', (req,res) => {
     let results = animals;
     if(req.query){
         results = filterByQuery(req.query, results);
