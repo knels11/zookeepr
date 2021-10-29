f9260a64e76a57fdd8d0bcd87f745bd57fba010b
kiehp <kierra.nelson@aol.com>
2021-10-29 19:39:19 -0400

    modularize code

---
+++ routes/apiRoutes/animalRoutes.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const {  filterByQuery, findById, createNewAnimal, validateAnimal  } = require('../../lib/animals');
+const {  animals  } = require('../../data/animals.json');
+
+//route
+//the function will take req.query as an arg and filter through animals accordingly
+router.get('/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+//param routes must come after GET route
+router.get('/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+router.post('/animals', (req, res) => {
+    //set id 1 greater than the current highest id value
+    req.body.id = animals.length.toString();
+    //if any data in req.body is incorrect send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.')
+    } else {
+    //add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
+    //req.body, where incoming content will be
+    res.json(animal);
+    }    
+});
+
+module.exports = router;
---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const router = require('express').Router();
+
+//route to respond with an html pg to display in the browser
+//route that serves the index.html page
+router.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+//this route takes us to /animals
+router.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'))
+});
+//this route takes us to the endpoint /zookeepers to display HTML content
+router.get('/zookeepers', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+module.exports = router;
